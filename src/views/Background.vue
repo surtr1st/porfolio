955ea563f6e2ba58488d947032dcd15521fb81a2
Ishi Tran <berugen1st@gmail.com>
2023-05-20 15:38:57 +0700

    add particulars for background and text appear eff

--- src/views/Background.vue
+++ src/views/Background.vue
@@ -6,37 +6,48 @@ const canvas = ref<HTMLCanvasElement | null>(null)
 
 onMounted(() => {
   if (canvas.value) {
-    const scene = new THREE.Scene()
-    const POV = 75
+    const POV = 90
     const ASPECT_RATIO = window.innerWidth / window.innerHeight
     const VIEW_FRUSTUM = [0.1, 1000]
+    const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(POV, ASPECT_RATIO, VIEW_FRUSTUM[0], VIEW_FRUSTUM[1])
     const renderer = new THREE.WebGLRenderer({
       canvas: canvas.value,
     })
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
-    camera.position.setZ(30)
+    camera.position.setZ(100)
     renderer.render(scene, camera)
 
     // Particular
     const particleGeometry = new THREE.BufferGeometry()
+    const particleGeometry2 = new THREE.BufferGeometry()
     const particleCount = 5000
     const positions = new Float32Array(particleCount * 3)
+    const positions2 = new Float32Array(particleCount * 3)
 
     // X-Y-Z
     for (let i = 0; i < particleCount * 3; i++) {
       positions[i] = (Math.random() - 0.5) * 1000
+      positions2[i] = (Math.random() - 0.5) * 1000
     }
     particleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    particleGeometry2.setAttribute('position', new THREE.BufferAttribute(positions2, 3))
 
     const pointsMaterial = new THREE.PointsMaterial({
-      size: 0.5,
+      size: 0.7,
       transparent: true,
       color: 0x686de0,
     })
+    const pointsDiffMaterial = new THREE.PointsMaterial({
+      size: 0.5,
+      transparent: true,
+      color: 0x3d65f5,
+    })
     const particleMesh = new THREE.Points(particleGeometry, pointsMaterial)
+    const particleMeshDiff = new THREE.Points(particleGeometry2, pointsDiffMaterial)
     scene.add(particleMesh)
+    scene.add(particleMeshDiff)
 
     // Lights
     const pointLight = new THREE.PointLight(0xffffff)
@@ -48,8 +59,6 @@ onMounted(() => {
     // Scroll Animation
     function moveCamera() {
       const t = document.body.getBoundingClientRect().top
-
-      particleMesh.translateZ(0.5)
 
       camera.position.z = t * -0.1
       camera.position.x = t * -0.002
@@ -66,6 +75,10 @@ onMounted(() => {
       particleMesh.rotation.x += 0.001
       particleMesh.rotation.y += 0.0001
       particleMesh.rotation.z += 0.0001
+
+      particleMeshDiff.rotation.x -= 0.001
+      particleMeshDiff.rotation.y -= 0.001
+      particleMeshDiff.rotation.z -= 0.001
 
       renderer.render(scene, camera)
     }
--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -10,13 +10,78 @@ import LightExtraHeading from '@/components/LightExtraHeading.vue'
     <Flex class="h-full w-[1090px] justify-center items-center">
       <Flex class="flex-col">
         <Flex>
-          <SuperExtraHeading class="font-bold">HELLO</SuperExtraHeading>
+          <Transition
+            appear
+            name="fade-up"
+          >
+            <SuperExtraHeading class="font-bold">HELLO</SuperExtraHeading>
+          </Transition>
           <Flex class="items-end">
-            <LightExtraHeading class="font-extralight italic mb-[11px]">AND</LightExtraHeading>
+            <Transition
+              appear
+              name="fade-up"
+            >
+              <LightExtraHeading class="font-extralight italic mb-[11px]">AND</LightExtraHeading>
+            </Transition>
           </Flex>
         </Flex>
-        <LightExtraHeading class="font-semibold">WELCOME TO MY PORTFOLIO</LightExtraHeading>
+        <Transition
+          appear
+          name="fade-down"
+        >
+          <LightExtraHeading class="font-semibold">WELCOME TO MY PORTFOLIO</LightExtraHeading>
+        </Transition>
       </Flex>
     </Flex>
   </Page>
 </template>
+
+<style scoped>
+@keyframes slide-fade-up {
+  from {
+    opacity: 0;
+    transform: translateY(75%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+}
+@keyframes slide-fade-down {
+  from {
+    opacity: 0;
+    transform: translateY(-75%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+}
+.fade-up-enter-active,
+.fade-up-leave-active {
+  animation: slide-fade-up 450ms;
+  animation-fill-mode: forwards;
+  animation-timing-function: cubic-bezier(0.62, -0.01, 0.09, 0.96);
+}
+
+.fade-up-enter-from,
+.fade-up-leave-to {
+  animation: slide-fade-up 350ms;
+  animation-fill-mode: forwards;
+  animation-timing-function: cubic-bezier(1, 0.01, 0.26, 0.82);
+}
+
+.fade-down-enter-active,
+.fade-down-leave-active {
+  animation: slide-fade-up 450ms;
+  animation-fill-mode: forwards;
+  animation-timing-function: cubic-bezier(0.62, -0.01, 0.09, 0.96);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  animation: slide-fade-up 350ms;
+  animation-fill-mode: forwards;
+  animation-timing-function: cubic-bezier(1, 0.01, 0.26, 0.82);
+}
+</style>
