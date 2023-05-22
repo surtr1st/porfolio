b83f87fd6d432101b8cbfea521b3b1c3b5d366ca
Ishi Tran <berugen1st@gmail.com>
2023-05-22 07:24:50 +0700

    add skills view and remove some svg

---
+++ src/components/GridItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface IGridItem {
+  colSpan?:
+    | 'col-span-1'
+    | 'col-span-2'
+    | 'col-span-3'
+    | 'col-span-4'
+    | 'col-span-5'
+    | 'col-span-6'
+    | 'col-span-full'
+
+  rowSpan?:
+    | 'row-span-1'
+    | 'row-span-2'
+    | 'row-span-3'
+    | 'row-span-4'
+    | 'row-span-5'
+    | 'row-span-6'
+    | 'row-span-full'
+}
+const { rowSpan, colSpan } = defineProps<IGridItem>()
+
+const className = `${rowSpan} ${colSpan}`
+</script>
+
+<template>
+  <div :class="className">
+    <slot />
+  </div>
+</template>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,8 +1,71 @@
 <script setup lang="ts">
-import Page from "../components/Page.vue";
+import Page from '@/components/Page.vue'
+import LightExtraHeading from '@/components/LightExtraHeading.vue'
+import HTML5Logo from '@/components/logo/HTML5Logo.vue'
+import CSS3Logo from '@/components/logo/CSS3Logo.vue'
+import JavaScriptLogo from '@/components/logo/JavaScriptLogo.vue'
+import TypeScriptLogo from '@/components/logo/TypeScriptLogo.vue'
+import JavaLogo from '@/components/logo/JavaLogo.vue'
+import Box from '@/components/Box.vue'
+import Flex from '@/components/Flex.vue'
+import Grid from '@/components/Grid.vue'
+import GridItem from '@/components/GridItem.vue'
 </script>
 
 <template>
-  <Page :background="600">
+  <Page class="w-full bg-none">
+    <Grid
+      rows="grid-rows-6"
+      cols="grid-cols-1"
+      class="h-full"
+    >
+      <GridItem
+        row-span="row-span-2"
+        col-span="col-span-full"
+      >
+        <Box class="flex justify-center items-center h-full">
+          <Transition
+            appear
+            name="fade-up"
+          >
+            <LightExtraHeading class="font-extrabold fancy hover:cursor-pointer"
+              >SKILLS</LightExtraHeading
+            >
+          </Transition>
+        </Box>
+      </GridItem>
+      <GridItem
+        row-span="row-span-3"
+        col-span="col-span-full"
+      >
+        <Flex class="justify-evenly items-center h-full">
+          <Box
+            class="grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
+          >
+            <HTML5Logo />
+          </Box>
+          <Box
+            class="grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
+          >
+            <CSS3Logo />
+          </Box>
+          <Box
+            class="grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
+          >
+            <JavaScriptLogo />
+          </Box>
+          <Box
+            class="grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
+          >
+            <TypeScriptLogo />
+          </Box>
+          <Box
+            class="grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
+          >
+            <JavaLogo />
+          </Box>
+        </Flex>
+      </GridItem>
+    </Grid>
   </Page>
 </template>
