0b069ed2903a411ffe7339a7435f5f3d155b3658
Ishi Tran <berugen1st@gmail.com>
2023-05-23 09:26:26 +0700

    reduce component define in a list

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -11,6 +11,29 @@ import Flex from '@/components/Flex.vue'
 import Grid from '@/components/Grid.vue'
 import GridItem from '@/components/GridItem.vue'
 import Paragraph from '@/components/Paragraph.vue'
+
+const contents = [
+  {
+    logo: HTML5Logo,
+    label: 'HTML',
+  },
+  {
+    logo: CSS3Logo,
+    label: 'CSS',
+  },
+  {
+    logo: JavaScriptLogo,
+    label: 'JavaScript',
+  },
+  {
+    logo: TypeScriptLogo,
+    label: 'TypeScript',
+  },
+  {
+    logo: JavaLogo,
+    label: 'Java',
+  },
+]
 </script>
 
 <template>
@@ -41,35 +64,11 @@ import Paragraph from '@/components/Paragraph.vue'
       >
         <Flex class="justify-evenly items-center h-full">
           <Box
+            v-for="content in contents"
             class="logo text-center grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
           >
-            <HTML5Logo />
-            <Paragraph>HTML</Paragraph>
-          </Box>
-          <Box
-            class="logo text-center grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
-          >
-            <CSS3Logo />
-            <Paragraph>CSS</Paragraph>
-          </Box>
-          <Box
-            class="logo text-center grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
-          >
-            <JavaScriptLogo />
-            <Paragraph>JavaScript</Paragraph>
-          </Box>
-          <Box
-            class="logo text-center grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-all hover:cursor-pointer hover:grayscale-0"
-          >
-            <TypeScriptLogo />
-            <Paragraph>TypeScript</Paragraph>
-          </Box>
-          <Box
-            ref="box"
-            class="logo text-center grayscale w-[150px] h-[150px] rounded-xl p-2 border border-none bg-none transition-transform hover:cursor-pointer hover:grayscale-0"
-          >
-            <JavaLogo class="transition-transform" />
-            <Paragraph>Java</Paragraph>
+            <component :is="content.logo"></component>
+            <Paragraph>{{ content.label }}</Paragraph>
           </Box>
         </Flex>
       </GridItem>
